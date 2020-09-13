7153230a501335288dcd88755943245282f5174a
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-09-13 11:00:29 +0200

    Acción para iniciar sesión con Vuex

---
+++ src/store/modules/authentication/actions.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+
+// eslint-disable-next-line import/prefer-default-export
+export async function signIn(context, user) {
+  try {
+    context.commit('setLoading', true, { root: true });
+    await Vue.axios({
+      method: 'POST',
+      url: '/login',
+      data: user,
+    });
+    context.commit('setUser', user);
+  } catch (e) {
+    context.commit('authError', e.message);
+  } finally {
+    context.commit('setLoading', false, { root: true });
+  }
+}
