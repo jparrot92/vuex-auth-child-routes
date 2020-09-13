62e392149f1b8463b52ce0b218bd5526bd1f28a1
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-09-13 12:53:45 +0200

    Desarrollar navegaciones para invitados y usuarios identificados

---
+++ src/components/Navigations/Logged.vue
@@ -0,0 +1,21 @@
+<template>
+    <b-nav tabs>
+        <b-nav-item exact to="/secret">Página secreta</b-nav-item>
+        <b-nav-item exact to="/secret/notes">Notas secretas</b-nav-item>
+        <b-nav-item @click="signOff">Cerrar sesión</b-nav-item>
+    </b-nav>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+
+export default {
+  methods: {
+    ...mapMutations('auth', ['logout']),
+    signOff() {
+      this.logout();
+      this.$router.push('/login');
+    },
+  },
+};
+</script>
