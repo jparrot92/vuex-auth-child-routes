0632323191ca149a5a62466dcd974b438d28e356
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-10-12 16:49:44 +0200

    Crear View Login y utilizar componente login form

--- src/components/Navigations/Guest.vue
+++ src/components/Navigations/Guest.vue
@@ -1,5 +1,5 @@
 <template>
     <b-nav tabs>
-        <b-nav-item exact to="/login">Iniciar sesión</b-nav-item>
+        <b-nav-item exact exact-active-class="active" to="/login">Iniciar sesión</b-nav-item>
     </b-nav>
 </template>
---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <login-form :user="user"></login-form>
+  </div>
+</template>
+
+<script>
+import LoginForm from '@/components/Authentication/Login.vue';
+
+export default {
+  components: {
+    LoginForm,
+  },
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+};
+</script>
