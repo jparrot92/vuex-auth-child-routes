d967c4f3aaea721eaff5cd696f2ecfeca8fc6669
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-10-12 10:37:24 +0200

    Extender VeeValidate con validación para fortaleza de password

--- src/plugins/vee-validate.js
+++ src/plugins/vee-validate.js
@@ -12,6 +12,15 @@ Object.keys(rules).forEach((rule) => {
 
 localize('es', es);
 
+extend('strength_password', {
+  message: (field) => `El campo ${field} debe contener al menos: 1 letra mayúscula, 1 letra minúscula, 1 número y un carácter especial (por ejemplo,. _ &? Etc.)`,
+  validate: (value) => {
+    // eslint-disable-next-line no-useless-escape
+    const strongRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})');
+    return strongRegex.test(value);
+  },
+});
+
 // Install components globally
 Vue.component('ValidationObserver', ValidationObserver);
 Vue.component('ValidationProvider', ValidationProvider);
