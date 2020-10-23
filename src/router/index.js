6998a3e96cb77de7349363ecca8b07b8b1f9c3a1
Jaume Parrot Altisent <jaumeparrotaltisent@MacBook-Pro-de-Jaume.local>
2020-10-24 01:05:36 +0200

    Evento BeforeEach de VueRouter para controlar rutas públicas y privadas

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
+import store from '../store';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
+});
+
+// eslint-disable-next-line consistent-return
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  const { isLogged } = store.state.auth;
+  if (!requiresAuth && isLogged && to.path === '/login') {
+    return next('/secret');
+  }
+  if (requiresAuth && !isLogged) {
+    next('/login');
+  } else {
+    next();
+  }
 });
 
 export default router;
